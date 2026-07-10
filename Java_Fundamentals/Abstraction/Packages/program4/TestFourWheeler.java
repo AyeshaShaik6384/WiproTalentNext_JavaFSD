5b4cfe7b37c909c5d230a7805ae7666d6a66533f
Ayesha <ayeshashaik6384@gmail.com>
2026-07-10 20:43:37 +0530

    Updated

---
+++ Java_Fundamentals/Abstraction/Packages/program4/TestFourWheeler.java
@@ -0,0 +1,27 @@
+package com.automobile.fourwheeler;
+
+public class TestFourWheeler {
+
+    public static void main(String[] args) {
+
+        Login logan = new Login();
+
+        System.out.println("-----Logan-----");
+        System.out.println("Model : " + logan.getModelName());
+        System.out.println("Registration : " + logan.getRegistrationNumber());
+        System.out.println("Owner : " + logan.getOwnerName());
+        System.out.println("Speed : " + logan.speed() + " km/h");
+        System.out.println("GPS : " + (logan.gps() == 1 ? "Available" : "Not Available"));
+
+        System.out.println();
+
+        Ford ford = new Ford();
+
+        System.out.println("-----Ford-----");
+        System.out.println("Model : " + ford.getModelName());
+        System.out.println("Registration : " + ford.getRegistrationNumber());
+        System.out.println("Owner : " + ford.getOwnerName());
+        System.out.println("Speed : " + ford.speed() + " km/h");
+        System.out.println("Temperature : " + ford.tempControl() + "°C");
+    }
+}
